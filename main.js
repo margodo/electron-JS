7c53bf69f51ebca164194c298122355fd3b99f9c
margodo <margaritadoand@gmail.com>
2025-07-09 22:22:10 +0200

    setting the window

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log('Hi')
+const path = require('path');
+const {app, BrowserWindow} = require('electron');
+
+const isDev = process.env.NODE_ENV !== 'development';
+const isMac = process.platform === 'darwin';
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Image Reaizer',
+        width: isDev ? 1000 : 5000,
+        height: 600
+    });
+
+    // Open devtools if in dev env
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (!isMac) {
+        app.quit()
+    }
+})
